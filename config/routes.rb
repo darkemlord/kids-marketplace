7bc6bd68058d6f2c3b44256323ec937a088c9937
Louis Yasuhiro <lyasuhir@gmail.com>
2021-11-15 17:58:05 +0900

    added routes. need to check if correct

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  resources :toys, only: %i[index show new create] do
+    resources :bookings, only: %i[create update]
+  end
+  resources :bookings, only: %i[index]
 end
