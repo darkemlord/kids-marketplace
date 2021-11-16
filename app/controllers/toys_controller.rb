5dba757389696c0cd90bafaeaffb65346acdd1bd
Anju Mogi <anju.mogi@gmail.com>
2021-11-16 14:06:12 +0900

    Updated show policy

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -2,5 +2,6 @@ class ToysController < ApplicationController
 
   def show
     @toy = Toy.find(params[:id])
+    authorize @toy
   end
 end
--- app/policies/toy_policy.rb
+++ app/policies/toy_policy.rb
@@ -3,5 +3,9 @@ class ToyPolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+
+    def show?
+      true
+    end
   end
 end
