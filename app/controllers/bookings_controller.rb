a8a0090a2c3b64ddaa1f95269270cf65d8e51ad6
emanuel caro <darkemlord@gmail.com>
2021-11-16 11:43:55 +0900

    new and create method created still need a test

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,23 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    raise
+  end
+
+  def create
+    @booking = Booking.new(bookings_params)
+    @booking.toy = @toy
+    if @booking.save
+      redirect_to toy_path(@toy)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bookings_params
+    params.require(:booking).permit(:start_date, :end_date, :delivery_option, :user_id, :toy_id)
+  end
+
 end
