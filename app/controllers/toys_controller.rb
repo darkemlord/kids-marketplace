32324839964079669846f8137fd6c32b96be0b53
gareth perilli <garethperilli@gmail.com>
2021-11-16 17:31:32 +0900

    primitive toy index page

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -1,2 +1,12 @@
 class ToysController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @toys = policy_scope(Toy)
+  end
+
+  def show
+    @toy = Toy.find(params[:id])
+  end
+
 end
