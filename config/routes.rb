e5c85a1eee80b2ef33b87b67d96fe821d7b4898b
emanuel caro <darkemlord@gmail.com>
2021-11-16 15:11:54 +0900

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :toys, only: %i[index show new create] do
-    resources :bookings, only: %i[update new create]
+    resources :bookings, only: %i[new create]
   end
-  resources :bookings, only: %i[index]
+  resources :bookings, only: %i[index update]
 end
