f0ee043581e1168e5cd4c32ab6f13aa2272fa901
Louis Yasuhiro <lyasuhir@gmail.com>
2021-11-18 10:51:34 +0900

    changed some enums from symbols to strings for display

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :toy
 
   enum approval_status: { approved: 0, pending: 1, denied: 2 }
-  enum delivery_option: { deliver: 0, pick_up: 1 }
+  enum delivery_option: { "Delivery": 0, "Pick Up": 1 }
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :delivery_option, presence: true
